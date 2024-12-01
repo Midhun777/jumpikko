a0d76b7570f266b14e970e31d119579d87eacd55
Midhun Mathew <midmat4828@gmail.com>
2024-12-01 23:36:17 +0530

    player

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let player = document.getElementById("player");
 let blockDiv = document.getElementById("block");
 
-function jump(){
-    player.classList.add("jump-class");
-    setTimeout(function(){
+function jump() {
+
+    if (player.classList != "jump-class") {
+        player.classList.add("jump-class");
+    }
+    setTimeout(function () {
         player.classList.remove("jump-class")
-    },500)
+    }, 500)
 }
+
+var checkDead = setInterval(function () {
+    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(blockDiv).getPropertyValue("left"));
+    console.log(playerTop);
+    console.log(blockLeft);
+
+    // if(blockLeft>50 && blockLeft<=80 && playerTop>=330){
+    //     blockDiv.style.animation="none";
+    //     blockDiv.style.display="none";
+    //     alert("oombi!")
+    // }
+}, 10)
